8e79b9b28f0e1ad83792c42f960781951ae95db7
tnd <tnd>
2024-11-06 12:53:13 +0700

    component header

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HeaderComponent } from './header.component';
+import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
+import {PanelModule} from 'primeng/panel';
+import {SplitButtonModule} from 'primeng/splitbutton';
+import {ToolbarModule} from 'primeng/toolbar';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    ButtonModule,
+    InputTextModule,
+    PanelModule,
+    SplitButtonModule,
+    ToolbarModule
+  ],
+  exports:[HeaderComponent]
 })
 export class HeaderModule { }

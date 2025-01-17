1564ba7de21d3c2851a209e05f8217f2fcb7e88b
tnd <tnd>
2025-01-17 15:27:34 +0700

    add multil input

--- src/app/medicalexamv1/medicalexamv1.component.ts
+++ src/app/medicalexamv1/medicalexamv1.component.ts
@@ -33,7 +33,6 @@ export class Medicalexamv1Component implements OnInit, OnDestroy{
       timeRegister: new FormControl(this.dataDialog.timeRegister),
       status: new FormControl(this.dataDialog.status),
       // symptons: new FormArray([new FormControl('')]),
-      typeOfMedicine: new FormControl(this.dataDialog.typeOfMedicine,[Validators.required]),
       dayOfExamination: new FormControl(this.dataDialog.dateRegister),
       money: new FormControl(this.dataDialog.money),
     });
@@ -54,44 +53,66 @@ export class Medicalexamv1Component implements OnInit, OnDestroy{
   }
 
   save(){
-  //   if(this.sMedicalExamForm.valid && this.typeMedicines.valid){
+    console.log(this.sMedicalExamForm);
+    console.log(this.typeOfMedicineForm);
+    
+    
+     if(this.sMedicalExamForm.valid && this.typeOfMedicineForm.valid){
   
   const values = this.symptons.map((control:FormControl) => control.value);
   console.log('values', values);
   
   //     let symptons = this.typeMedicines
-  //   let medicalExam = {
-  //     id:this.f['id'].value==null?0:this.f['id'].value,
-  //     fullName: this.f['fullName'].value,
-  //     status: 1,
-  //     sympton: this.f['sympton'].value,
-  //     typeOfMedicine: this.f['typeOfMedicine'].value,
-  //     dayOfExamination: this.f['dayOfExamination'].value,
-  //     medical:{
-  //       id:this.dataDialog.idSchedule
-  //     },
-  //     money: this.f['money'].value
-  //   }
-  //    this.medicalServie.addMedicalExam(medicalExam).subscribe({
-  //      next: data => {
-  //        this.messageService.add({severity:'success', summary:'Success',detail:'Save successfully ' + data.data.fullName});
-  //        setTimeout(() => {
-  //          this.ref.close();
-  //           this.router.navigateByUrl('/',{skipLocationChange:true}).then(() =>{
-  //             this.router.navigate(['/listregister']);
-  //           })
-  //        },500);
-  //      }
-  //    })
-  // }else{
-  //   this.messageService.add({severity: 'error', summary: 'Lỗi', detail: 'Field not blank!'});
-  // }
-  console.log('formsave', this.typeOfMedicineForm.get('typeMedicines')?.value);
-  console.log('formmoney', this.typeOfMedicineForm.get('moneys')?.value);
+    let medicalExam = {
+      id:this.f['id'].value==null?0:this.f['id'].value,
+      fullName: this.f['fullName'].value,
+      status: 1,
+      sympton: this.symptonsValue ,
+      typeOfMedicine: this.typeMedicineValue,
+      dayOfExamination: this.f['dayOfExamination'].value,
+      medical:{
+        id:this.dataDialog.idSchedule
+      },
+      money: this.moneysValue
+    }
 
-  
+    console.log('medicalExam',medicalExam);
+    
+     this.medicalServie.addMedicalExam(medicalExam).subscribe({
+       next: data => {
+         this.messageService.add({severity:'success', summary:'Success',detail:'Save successfully ' + data.data.fullName});
+         setTimeout(() => {
+           this.ref.close();
+            this.router.navigateByUrl('/',{skipLocationChange:true}).then(() =>{
+              this.router.navigate(['/listregister']);
+            })
+         },500);
+       }
+     })
+  }else{
+    this.messageService.add({severity: 'error', summary: 'Lỗi', detail: 'Field not blank!'});
+  }
 
+  console.log('totalMoney',this.totalMoney);
   
+  }
+
+  get typeMedicineValue(): string{
+    return Object.keys(this.typeMedicines).map(key => {
+      return  this.typeMedicines[key]?.value ;
+    }).join(',');
+  }
+
+  get moneysValue(): string{
+    return Object.keys(this.moneys).map(key => {
+      return  this.moneys[key]?.value ;
+    }).join(',');
+  }
+
+  get symptonsValue(): string{
+    return Object.keys(this.symptons).map(key => {
+      return  this.symptons[key]?.value ;
+    }).join(',');
   }
 
   close(){
@@ -115,11 +136,17 @@ export class Medicalexamv1Component implements OnInit, OnDestroy{
   }
 
   get typeMedicines(){
-    return (this.typeOfMedicineForm.get('typeMedicines') as FormArray);
+    return (this.typeOfMedicineForm.get('typeMedicines') as any).controls;
   }
 
   get moneys(){
     return (this.typeOfMedicineForm.get('moneys') as any).controls;
+  }
+
+  get totalMoney(){
+    return Object.keys(this.moneys).map(key => {
+      return  this.moneys[key]?.value ;
+    }).reduce((total,cur) => total + cur,0);
   }
 
   addTypeMedicine(){
@@ -131,5 +158,25 @@ export class Medicalexamv1Component implements OnInit, OnDestroy{
     this.moneys.push(new FormControl('')); // Thêm item mới vào moneys
   }
 
+  onChange(e: Event){
+    this.sMedicalExamForm.patchValue({
+      money: this.totalMoney
+    });
+    
+  }
+
+  // Xử lý sự kiện keydown
+  onKeyDown(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      this.addTypeMedicine();
+      this.addMoney();
+    }
+  }
+
+  removeTypeMedicine(i: any){
+  
+  }
 
 }
+
+

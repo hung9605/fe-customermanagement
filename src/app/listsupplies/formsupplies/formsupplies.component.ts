403ccb32c3eeb0bce675d52ad08aa2dd477050f2
tnd <tnd>
2025-02-17 17:50:37 +0700

    u

--- src/app/listsupplies/formsupplies/formsupplies.component.ts
+++ src/app/listsupplies/formsupplies/formsupplies.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { SupppliesService } from '../suppplies.service';
 
 @Component({
   selector: 'app-formsupplies',
@@ -7,6 +8,10 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrl: './formsupplies.component.scss'
 })
 export class FormsuppliesComponent implements OnInit {
+
+  file: any;
+
+  constructor(private suppliesService: SupppliesService){}
 
   suppliesForm !: FormGroup;
 
@@ -19,11 +24,22 @@ export class FormsuppliesComponent implements OnInit {
   }
 
   addsupplies(){
-
+    this.suppliesService.upload(this.file).subscribe({
+      next: response => {
+        console.log('Tệp đã được tải lên thành công:', response);
+      },
+      error: err => {
+        console.error('Lỗi khi tải tệp lên:', err);
+      }
+    })
   }
 
   onUpload(e: any){
-    console.log('event', e);
+    
+    this.file = e.files[0];
+
+
+   
     
   }
 
--- src/app/listsupplies/suppplies.service.ts
+++ src/app/listsupplies/suppplies.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SupppliesService {
+  url = environment.urlApi +"/upload";
+  
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  upload(file : any):Observable<any>{
+
+    // Gửi tệp lên backend bằng HTTP (sử dụng HttpClient)
+    const formData = new FormData();
+    formData.append('file', file, file.name);
+      return this.http.post(`${this.url}/file`,formData);
+    }
 }

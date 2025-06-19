58cf22e8ab3bb5f6bce0f604d6804badac81eb56
hung9605 <hung9605@gmail.com>
2025-06-19 00:40:20 +0000

    u

--- src/app/common/utils/only-number.directive.ts
+++ src/app/common/utils/only-number.directive.ts
@@ -13,7 +13,9 @@ export class OnlyNumberDirective {
     ];
     const numberCodes = [
       'Digit0', 'Digit1', 'Digit2', 'Digit3', 'Digit4',
-      'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9'
+      'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9',
+      'Numpad0', 'Numpad1', 'Numpad2', 'Numpad3', 'Numpad4',
+      'Numpad5', 'Numpad6', 'Numpad7', 'Numpad8', 'Numpad9'
     ];
 
     console.log('event.code', numberCodes.includes(event.code));

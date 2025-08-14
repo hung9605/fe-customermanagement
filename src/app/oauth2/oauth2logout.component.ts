3893bcfbfe5c2f1019e83f60f263303eaa4769ba
nd.tuan <nd.tuan>
2025-08-14 15:16:21 +0700

    update logout

--- src/app/oauth2/oauth2logout.component.ts
+++ src/app/oauth2/oauth2logout.component.ts
@@ -17,22 +17,9 @@ export default class Oauth2LogoutComponent implements OnInit{
         console.error('Không tìm thấy id_token');
         return;
       }
-    const params = new HttpParams().set('id_token_hint', idToken).set('post_logout_redirect_uri','http://localhost:4200');
-        localStorage.clear();
-        this.http.get('http://localhost:9005/oauth2/logout-rp', {
-  params,
-  observe: 'response'  // để lấy toàn bộ HttpResponse, bao gồm headers
-}).subscribe({
-  next: (resp) => {
-    console.log(resp);
-    // kiểm tra header Location
-    const redirectUrl = resp.headers.get('Location');
-    if (redirectUrl) {
-      window.location.href = redirectUrl; // redirect FE
-    }
-  },
-  error: (err) => console.error('Logout failed', err)
-});
+      localStorage.clear();
+      window.location.href = `http://localhost:9005/oauth2/logout-rp?id_token_hint=${idToken}
+                                &post_logout_redirect_uri=http://localhost:4200`;
 
 
     }

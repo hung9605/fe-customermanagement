75e9e737824d927ff6b774177f77cb04bfd7ec55
nguyentuan <hung9605@gmail.com>
2025-09-21 00:40:48 +0700

    u

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
-export class UserComponent {
+export class UserComponent implements OnInit,OnDestroy {
+
+  isLoading = true;
+  sUser: any[] = [];
+  row = environment.rowPanigator;
+  searchInput:any;
+  readonly columnTitles = [
+           {title:'STT',class:'text-center text-black-alpha-90',classHeader:'w-1', field: 'index'}
+          ,{title:'Username',class:'text-left text-black-alpha-90',classHeader:'w-3',field:'username'}
+          ,{title:'Password',class:'text-right text-indigo-600',classHeader:'w-5',field:'password'}
+          ,{title:'Status',class:'text-center text-indigo-600',classHeader:'w-1',field:'status'}
+          ,{title:'Action',class:'text-center text-indigo-600',classHeader:'w-2',field:'status'}
+        ];
+
+  ngOnInit(): void {
+    this.isLoading = false;
+    
+  }
+
+  ngOnDestroy(): void {
+    
+  }
+
+  search(dt1: any){
+
+  }
+
+    searchResult(e:KeyboardEvent,dt1:any){
+    if (e.key === 'Enter') {
+      this.search(dt1);
+    }
+  }
+
+  exportToExcel(){
+
+  }
+
+
 
 }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,18 +1,49 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MenubarModule } from 'primeng/menubar';
+import { HttpClientModule } from '@angular/common/http';
+import { PanelModule } from 'primeng/panel';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import {TableModule} from 'primeng/table';
+import {PaginatorModule} from 'primeng/paginator';
+import {CalendarModule} from 'primeng/calendar';
+import {ToastModule} from 'primeng/toast';
+import {TooltipModule} from 'primeng/tooltip';
+import {RouterModule } from '@angular/router';
+import {DialogModule} from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import {MessageService} from 'primeng/api';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { UserComponent } from './user.component';
 
 
 
 @NgModule({
-  declarations: [
-    UserComponent
-  ],
+  declarations: [UserComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MenubarModule,
+    HttpClientModule,
+    PaginatorModule,
+    PanelModule,
+    FormsModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    TableModule,
+    PaginatorModule,
+    RouterModule,
+    ButtonModule,
+    CalendarModule,
+    ToastModule,
+    TooltipModule,
+    DialogModule,
+    TableModule,
+    ProgressSpinnerModule
   ],
-  exports:[
-    UserComponent
-  ]
+  exports:[],
+  providers:[MessageService]
 })
+
+
 export class UserModule { }
